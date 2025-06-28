4ae0e84de7255fc856c94dbf726d92946a460202
Peter Hollender <peterhollender@gmail.com>
2025-06-28 09:00:39 -0700

    resolve merge conflict

--- mobile-application-openwater/babel.config.js
+++ mobile-application-openwater/babel.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-<<<<<<< HEAD
   presets: ['module:@react-native/babel-preset'],
-=======
-  presets: ["babel-preset-expo"],
-  plugins: ["react-native-reanimated/plugin"],
->>>>>>> main
 };
